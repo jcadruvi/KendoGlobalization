1f7035f4e26eac9d4d1ee6730a14144f43f45e6f
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-27 14:53:17 -0700

    Added some objects I need.

---
+++ KendoGlobal.Site/Models/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoGlobal.Models
+{
+    public class BaseViewModel
+    {
+        public string Culture { get; set; }
+    }
+}
