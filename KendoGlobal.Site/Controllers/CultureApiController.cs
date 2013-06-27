62618384c498c065cc6bcacc36f7346446b2697e
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-27 11:21:41 -0700

    Added the culture code.

---
+++ KendoGlobal.Site/Controllers/CultureApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KendoGlobal.Controllers
+{
+    public class CultureApiController : ApiController
+    {
+        private ICollection<KeyValuePair<string, string>> _cultures;
+        public CultureApiController()
+        {
+            _cultures = new Collection<KeyValuePair<string, string>>();
+            _cultures.Add(new KeyValuePair<string, string>("en-US", "English US"));
+            _cultures.Add(new KeyValuePair<string, string>("en-GB", "English GB"));
+        }
+        public IEnumerable<KeyValuePair<string, string>> GetCultures()
+        {
+            return _cultures;
+        }
+    }
+}
