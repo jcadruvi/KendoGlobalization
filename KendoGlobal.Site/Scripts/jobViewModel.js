2caba58676fd6d11e837bf2c53ac21f80ea948b0
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-27 10:23:05 -0700

    Start and End Date fields display correctly.

---
+++ KendoGlobal.Site/Scripts/jobViewModel.js
@@ -0,0 +1,54 @@
+﻿function jobViewModel() {
+    var self = {};
+
+    self.endDateData = null;
+    self.jobGridData = null;
+    self.id = null;
+    self.name = null;
+    self.startDateData = null;
+
+    var getSelectedJobId = function () {
+        var dataItem;
+        if (!self.jobGridData) {
+            return null;
+        }
+        dataItem = self.jobGridData.dataItem(self.jobGridData.select());
+        if (!dataItem) {
+            return null;
+        }
+        return dataItem.Id;
+    };
+
+    self.onJobGridChange = function () {
+        var postData = {};
+
+        postData.Id = getSelectedJobId();
+
+        $.ajax({
+            data: postData,
+            dataType: 'json',
+            success: function (result) {
+                self.id(result.Id);
+                self.name(result.Name);
+                if (self.endDateData) {
+                    self.endDateData.value(new Date(result.EndDate));
+                }
+                if (self.startDateData) {
+                    self.startDateData.value(new Date(result.StartDate));
+                }
+            },
+            type: 'GET',
+            url: 'api/JobApi/GetJob'
+        });
+    };
+
+    self.setObservables = function() {
+
+        self.id = ko.observable();
+        self.name = ko.observable();
+        
+        ko.applyBindings(self);
+    };
+
+    return self;
+}
