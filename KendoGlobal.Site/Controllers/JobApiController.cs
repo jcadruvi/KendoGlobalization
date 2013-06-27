0159de9116e4bcf32abe0e3db487f97579b68c07
Josh Cadruvi <jcadruvi@mobitor.com>
2013-06-27 09:51:32 -0700

    Id and Name fields display correctly.

--- KendoGlobal.Site/Controllers/JobApiController.cs
+++ KendoGlobal.Site/Controllers/JobApiController.cs
@@ -11,52 +11,60 @@ namespace Mvc4.Controllers
 {
     public class JobApiController : ApiController
     {
-        public IEnumerable<Job> GetJobs()
+        private ICollection<Job> _jobs; 
+        public JobApiController()
         {
-            ICollection<Job> jobs = new Collection<Job>();
-            jobs.Add(new Job
+            _jobs = new Collection<Job>();
+            _jobs.Add(new Job
             {
                 Id = 1,
                 Name = "Job 1",
                 StartDate = DateTime.Parse("01/01/2013"),
                 EndDate = DateTime.Parse("03/01/2013")
             });
-            jobs.Add(new Job
+            _jobs.Add(new Job
             {
                 Id = 2,
                 Name = "Job 2",
                 StartDate = DateTime.Parse("01/02/2013"),
                 EndDate = DateTime.Parse("03/02/2013")
             });
-            jobs.Add(new Job
+            _jobs.Add(new Job
             {
                 Id = 3,
                 Name = "Job 3",
                 StartDate = DateTime.Parse("01/03/2013"),
                 EndDate = DateTime.Parse("03/03/2013")
             });
-            jobs.Add(new Job
+            _jobs.Add(new Job
             {
                 Id = 4,
                 Name = "Job 4",
                 StartDate = DateTime.Parse("01/04/2013"),
                 EndDate = DateTime.Parse("03/04/2013")
             });
-            jobs.Add(new Job
+            _jobs.Add(new Job
             {
                 Id = 5,
                 Name = "Job 5",
                 StartDate = DateTime.Parse("01/05/2013"),
                 EndDate = DateTime.Parse("03/05/2013")
             });
-            jobs.Add(new Job
+            _jobs.Add(new Job
             {
                 Id = 6,
                 Name = "Job 6",
                 StartDate = DateTime.Parse("01/06/2013"),
                 EndDate = DateTime.Parse("03/06/2013")
             });
-            return jobs;
+        }
+        public Job GetJob(int id)
+        {
+            return _jobs.First(j => j.Id == id);
+        }
+        public IEnumerable<Job> GetJobs()
+        {
+            return _jobs;
         }
     }
 }
