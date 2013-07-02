810bcfd6ec4f59fcbe765352bef4c7fcae7d669f
Josh Cadruvi <jcadruvi@mobitor.com>
2013-07-02 16:39:02 -0700

    Added the JobViewModel.cs.

---
+++ KendoGlobal.Site/Models/JobViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoGlobal.Models
+{
+    public class JobViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+        public string Percent { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
