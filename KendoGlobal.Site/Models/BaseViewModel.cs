9543f367a99e2069188d08119e736159dbe69225
Josh Cadruvi <jcadruvi@mobitor.com>
2013-07-03 13:31:57 -0700

    Currency is working.

--- KendoGlobal.Site/Models/BaseViewModel.cs
+++ KendoGlobal.Site/Models/BaseViewModel.cs
@@ -9,12 +9,19 @@ namespace KendoGlobal.Models
     public class BaseViewModel
     {
         public string Culture { get; set; }
+        
+        public string CurrencyDecimalSeperator { get; set; }
+        public string CurrencyGroupSeperator { get; set; }
+        public string CurrencySymbol { get; set; }
         public string DateFormat { get; set; }
         
         public BaseViewModel()
         {
             CultureInfo cultureInfo = CultureInfo.CurrentCulture;
             Culture = cultureInfo.Name;
+            CurrencyDecimalSeperator = cultureInfo.NumberFormat.CurrencyDecimalSeparator;
+            CurrencyGroupSeperator = cultureInfo.NumberFormat.CurrencyGroupSeparator;
+            CurrencySymbol = cultureInfo.NumberFormat.CurrencySymbol;
             DateFormat = cultureInfo.DateTimeFormat.ShortDatePattern;
         }
     }
